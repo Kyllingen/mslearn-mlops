3758c556e73ed69bb808de0644300267dcb875d9
EHA <kyllingen@hotmail.com>
2023-08-08 20:58:05 +0200

    fixed linting issue

--- src/model/train.py
+++ src/model/train.py
@@ -27,6 +27,7 @@ def main(args):
     # train model
     train_model(args.reg_rate, X_train, X_test, y_train, y_test)
 
+
 def split_data(df):
     # split data
     X, y = df[['Pregnancies','PlasmaGlucose','DiastolicBloodPressure','TricepsThickness','SerumInsulin','BMI','DiabetesPedigree','Age']].values, df['Diabetic'].values
@@ -66,6 +67,7 @@ def parse_args():
 
     # return args
     return args
+
 
 # run script
 if __name__ == "__main__":
