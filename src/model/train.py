987221eb28527240115f7e4ecacf789e16fe435e
Kyllingen <kyllingen@hotmail.com>
2023-08-09 20:26:55 +0200

    update linter rules again (#7) (#8)
    * update linter rules again (#7)
    
    * Update train.py

--- src/model/train.py
+++ src/model/train.py
@@ -32,7 +32,7 @@ def split_data(df):
         'Pregnancies', 'PlasmaGlucose', 'DiastolicBloodPressure',
         'TricepsThickness', 'SerumInsulin', 'BMI',
         'DiabetesPedigree', 'Age']].values, df['Diabetic'].values
-
+ 
     X_train, X_test, y_train, y_test = train_test_split(
         X, y, test_size=0.30, random_state=0)
 
